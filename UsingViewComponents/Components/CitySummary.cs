7439ed76fc44375fb2b1224312ce1448451532bf
flashpoint1986@mail.ru <flashpoint1986@mail.ru>
2021-01-25 01:46:57 +0300

    ViewComponent

---
+++ UsingViewComponents/Components/CitySummary.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Components
+{
+    public class CitySummary : ViewComponent
+    {
+        private ICityRepository repository;
+        public CitySummary(ICityRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        //public string Invoke() => $"{repository.Cities.Count()} cities, {repository.Cities.Sum(c => c.Population)} people";
+        public IViewComponentResult Invoke() => View(new CityViewModel { Cities = repository.Cities.Count(), Population = repository.Cities.Sum(c => c.Population) });
+    }
+}
---
+++ UsingViewComponents/Components/PocoViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Components
+{
+    public class PocoViewComponent
+    {
+        private ICityRepository repository;
+        public PocoViewComponent(ICityRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public string Invoke() => $"{repository.Cities.Count()} cities, {repository.Cities.Sum(c => c.Population)} people";
+    }
+}
