23e7861a3d4da2b5ff396b54094a5550af237b3e
flashpoint1986@mail.ru <flashpoint1986@mail.ru>
2021-01-27 01:39:55 +0300

    завершена глава 22

--- UsingViewComponents/Components/CitySummary.cs
+++ UsingViewComponents/Components/CitySummary.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +18,18 @@ namespace UsingViewComponents.Components
         }
 
         //public string Invoke() => $"{repository.Cities.Count()} cities, {repository.Cities.Sum(c => c.Population)} people";
-        public IViewComponentResult Invoke() => View(new CityViewModel { Cities = repository.Cities.Count(), Population = repository.Cities.Sum(c => c.Population) });
+        //public IViewComponentResult Invoke() => View(new CityViewModel { Cities = repository.Cities.Count(), Population = repository.Cities.Sum(c => c.Population) });
+        //public IViewComponentResult Invoke() => new HtmlContentViewComponentResult(new HtmlString("This is a <h3><i>string</i></h3>"));
+        public IViewComponentResult Invoke(bool showList)
+        {
+            if (showList)
+            {
+                return View("CityList", repository.Cities);
+            }
+            else
+            {
+                return View(new CityViewModel { Cities = repository.Cities.Count(), Population = repository.Cities.Sum(c => c.Population) });
+            }
+        }
     }
 }
